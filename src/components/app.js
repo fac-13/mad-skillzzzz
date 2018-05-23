4486baa1bb2b63507734972ff5ec8c035eaafaf9
tspeed90 <tspeed90@gmail.com>
2018-05-23 16:48:13 +0100

    Pass Quiz categories to the App component

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Button from "./button";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizData: null,
+    }
+  }
 
   sayHi = () => {
-    console.log('hi!');
+    console.log(this.props.quizCategories);
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from "./components/app";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let quizzes = new Map();
+quizzes.set(17, "Science and Nature")
+  .set(26, "Celebrities")
+  .set(21, "Sports")
+  .set(27, "Animals")
+  .set(20, "Mythology")
+  .set(9, "General Knowledge");
+
+
+ReactDOM.render(<App quizCategories={quizzes} />, document.getElementById('root'));
