9b433ad6985cfffd88974de027a4000db9eb381a
tspeed90 <tspeed90@gmail.com>
2018-07-11 14:40:14 +0100

    adds actions and reducers

---
+++ src/actions/actions.js
@@ -0,0 +1,38 @@
+export const setQuizQuestions = data => {
+  return {
+    type: 'SET_QUIZ_QUESTIONS',
+    quizData: data
+  };
+};
+
+export const setSessionToken = token => {
+  return {
+    type: 'SET_SESSION_TOKEN',
+    sessionToken: token
+  };
+};
+
+export const incrementRightAnswers = () => {
+  return {
+    type: 'INCREMENT_RIGHT_ANSWERS'
+  };
+};
+
+export const updateCurrentQuestion = () => {
+  return {
+    type: 'UPDATE_CURRENT_QUESTION',
+    currentQuestion
+  };
+};
+
+export const markCategorySelected = () => {
+  return {
+    type: 'MARK_CATEGORY_SELECTED'
+  };
+};
+
+export const resetGame = () => {
+  return {
+    type: 'RESET_GAME'
+  };
+};
---
+++ src/reducers/reducers.js
@@ -0,0 +1,45 @@
+import {
+  setQuizQuestions,
+  setSessionToken,
+  incrementRightAnswers,
+  updateCurrentQuestion,
+  markCategorySelected,
+  resetGame
+} from '../actions/actions';
+
+export const initialState = {
+  sessionToken: null,
+  quizData: null,
+  rightAnswers: 0,
+  currentQuestion: 0,
+  categorySelected: false
+};
+
+export const updateState = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_QUIZ_QUESTIONS':
+      return Object.assign({}, state, {
+        quizData: action.quizData
+      });
+    case 'SET_SESSION_TOKEN':
+      return Object.assign({}, state, {
+        sessionToken: action.sessionToken
+      });
+    case 'INCREMENT_RIGHT_ANSWERS':
+      return Object.assign({}, state, {
+        rightAnswers: state.rightAnswers + 1
+      });
+    case 'UPDATE_CURRENT_QUESTION':
+      return Object.assign({}, state, {
+        currentQuestion: state.currentQuestion + 1
+      });
+    case 'MARK_CATEGORY_SELECTED':
+      return Object.assign({}, state, {
+        categorySelected: true
+      });
+    case 'RESET_GAME':
+      return Object.assign({}, initialState);
+    default:
+      return state;
+  }
+};
